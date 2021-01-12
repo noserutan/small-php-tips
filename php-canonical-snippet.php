c11238ac51b14bfd429fe5f2898b7d91db7307d3
noserutan <75997694+noserutan@users.noreply.github.com>
2021-01-12 22:25:24 +0700

    Initial commit

---
+++ php-canonical-snippet.php
@@ -0,0 +1,11 @@
+# Single line
+```
+<link rel='canonical' href='<?= (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on') ? "https://" : "http://".strtok($_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'], "?"); ?>'>
+```
+
+# Working with examples
+***example.com/***
+***example.com/page.html***
+***example.com/page.html/***
+***example.com/page.html?pr=something&sp=number***
+***example.com/page.html/?pr=something&sp=number***
